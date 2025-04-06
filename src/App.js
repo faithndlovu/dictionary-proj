e7a52b382dede415820006d6f9235af0ccb7c35f
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-04-06 18:14:13 +0200

    added dictonary api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import Dictionary from "./Dictionary";
 
 function App() {
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,24 @@
 import React, {useState} from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary(){
 let [keyword, setkeyword]=useState ("");
-    function search(event) {
+    
+function handleResponse(response){
+    console.log(response.data[0]);
+}
+
+function search(event) {
     event.preventDefault();
-    alert (`Searching for ${keyword}`);
- }
+
+ 
+let apiUrl= "https://api.shecodes.io/dictionary/v1/define?word=love&key=dof43157bf3aftdf9163bbd7407de4ba";
+axios.get(apiUrl).then(handleResponse);
+}
+
+ 
+ 
  function handlekeywordChange(event){
     setkeyword(event.target.value);
  }
