9ef5258ca4bae13f047676d52bc82bdedf0977e0
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-04-07 19:30:34 +0200

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,7 +4,8 @@ import "./Dictionary.css";
 
 export default function Dictionary(){
 let [keyword, setkeyword]=useState ("");
-    
+
+
 function handleResponse(response){
     console.log(response.data[0]);
 }
@@ -13,7 +14,7 @@ function search(event) {
     event.preventDefault();
 
  
-let apiUrl= "https://api.shecodes.io/dictionary/v1/define?word=love&key=dof43157bf3aftdf9163bbd7407de4ba";
+let apiUrl= "https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=dof43157bf3aftdf9163bbd7407de4ba";
 axios.get(apiUrl).then(handleResponse);
 }
 
