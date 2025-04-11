3a34e0197244b58723c3e29fc85003ca5437900f
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2025-04-11 20:38:12 +0200

    updated the meanig

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,7 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props){
 return (
     <div className="Meaning">
-    <h3>{props.meaning.partofspeech}</h3>
+    <h3 className="PartOfSpeech">This is a {props.meaning.partofspeech}</h3>
          {props.meaning.definitions.map(function (definition, index){
             return (
                 <div key={index}>
